28fe95b7e1bd0396c1105940980708bdfd3c5afd
ridhi2006 <oridhi19@gmail.com>
2025-01-23 01:59:41 +0530

    Solution to Concatenate Two Strings

--- Concatenate Two Strings@@676c039d80e1714600f67644/code.c
+++ Concatenate Two Strings@@676c039d80e1714600f67644/code.c
@@ -1,20 +1,35 @@
 #include <stdio.h>
 #include<string.h>
-int main(){
-    char str1[100],str2[100],result[200];
+void concatenateStrings(char str1[],char str2[],char result[]){
     int i=0,j=0;
-    fgets(str1,sizeof(str1),stdin);
-    while(str1[i]!='\0' && str1[i]!='\n'){
+    while(str1[i]!='\0'){
         result[i]=str1[i];
-        i++;
+        i++
     }
-    fgets(str1,sizeof(str2),stdin);
-    while(str2[j]!='\0' && str2[j]!='\n'){
+    while(str2[j]!='\0'){
         result[i]=str2[j];
         i++;
         j++;
     }
     result[i]='\0';
-    printf("%s\n",result);
+}
+int main(){
+    char str1[100],str2[100],result[200];
+    fgets(str1,sizeof(str1),stdin);
+    for(int i=0;str1[i]!=0;i++){
+        if(str1[i]=='\n'){
+            str1[i]='\0';
+            break;
+        }
+    }
+    fgets(str2,sizeof(str2),stdin);
+    for(int i=0;str2[i]!=0;i++){
+        if(str2[i]=='\n'){
+            str2[i]=='\0';
+            break;
+        }
+    }
+    concatenateStrings(str1,str2,result);
+    printf("%s\n",return);
     return 0;
 }
