21b516b0daf58d6c887c90a974c5b49f99d2e435
ridhi2006 <oridhi19@gmail.com>
2025-01-22 13:26:38 +0530

    Solution to Grade Evaluation

--- Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -4,18 +4,23 @@ int main(){
     scanf("%d",&grades);
     switch(grades){
         case 'A':
+        case 'a':
         printf("Excellent\n");
         break;
         case 'B':
+        case 'b':
         printf("Good\n");
         break;
         case 'C':
+        case 'c':
         printf("Average\n");
         break;
         case 'D':
+        case 'd':
         printf("Below Average\n");
         break;
         case 'F':
+        case 'f':
         printf("Fail\n");
         break;
         default:
