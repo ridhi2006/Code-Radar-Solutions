525b5a41b78e6a15aa92c89e5c69a409e5260534
ridhi2006 <oridhi19@gmail.com>
2025-01-28 14:36:38 +0530

    Solution to Print an Increasing Numeric Pyramid

--- Print an Increasing Numeric Pyramid@@67877e1818d53eb724f435d1/code.c
+++ Print an Increasing Numeric Pyramid@@67877e1818d53eb724f435d1/code.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 int main(){
-    int i,j,rows,space;
+    int i,j,rows,spaces;
     scanf("%d",&rows);
     for(i=1;i<=rows;i++){
-        printf(" ");
+        for(spaces=1;spaces<=n-i;spaces++){
+        printf(" ");}
         for(j=1;j<=i;j++){
             printf("%d ",j);
         }
-        printf(" \n");
+        printf("\n");
     }
     return 0;
 }
