66a9e047d195bd74e4a0c90135f20b48d4f365e1
Ridhi <oridhi19@gmail.com>
2025-03-01 23:01:28 +0530

    Solution to Find the Median of an Array

--- Find the Median of an Array@@676c02002dbc8c3e7a280b8c/code.c
+++ Find the Median of an Array@@676c02002dbc8c3e7a280b8c/code.c
@@ -1,18 +1,36 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+
+int main() {
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
+    
     int arr[n];
-    for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
+    
+    // Input array elements
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
-    int median;
-    for(int i=0;i<n/2;i++){
-        if(n%2==1){
-            median=arr[n/2];
+
+    // Sorting the array using Bubble Sort
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] > arr[j]) {  // Swap if out of order
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
         }
-        
     }
-    printf("%d",median);
+
+    // Finding the median
+    double median;
+    if (n % 2 == 1) {
+        median = arr[n / 2];  // Middle element for odd n
+    } else {
+        median = (arr[n / 2 - 1] + arr[n / 2]) / 2.0;  // Average of two middle elements for even n
+    }
+
+    printf("Median: %.1f\n", median);
+    
     return 0;
 }
