a18064d5349bd5b3bf0beaac88b4537be0abbbb7
Ridhi <oridhi19@gmail.com>
2025-03-28 16:25:34 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -4,7 +4,7 @@ int a=0,b=1,next;
 
 for(int i=0;i<n;i++){
     printf("%d",a);
-    if(i<n-1){
+    if(i<n-1){     //taakiii spaces numbers ke beech m aaye naakii end m
         printf(" ");
     }
     next=a+b;
