ce5b71060c115d23e1644030f28c46fc9938f52a
Ridhi <oridhi19@gmail.com>
2025-04-07 19:26:48 +0530

    Solution to Calculate Average Marks of Students

---
+++ Calculate Average Marks of Students@@67877d61dc107486ad5c2743/code.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+struct Student{
+    int rollNumber;
+    char name[100];
+    float marks;
+};
+int main(){
+    int n;
+    scanf("%d\n",&n);
+
+    struct Student students[n];
+
+    for(int i=0;i<n;i++){
+        scanf("%d %s %f\n",&students[i].rollNumber,&students[i].name,&students[i].marks);
+    }
+    for(int i=0;i<n;i++){
+        
+    }
+}
