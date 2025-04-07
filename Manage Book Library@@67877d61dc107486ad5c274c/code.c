0f1cf541477821636abb20d585dc9a3428d2812f
Ridhi <oridhi19@gmail.com>
2025-04-08 01:01:14 +0530

    Solution to Manage Book Library

--- Manage Book Library@@67877d61dc107486ad5c274c/code.c
+++ Manage Book Library@@67877d61dc107486ad5c274c/code.c
@@ -1,31 +1,40 @@
-#include<stdio.h>
-#include<string.h>
-struct Book{
+#include <stdio.h>
+#include <string.h>
+
+struct Book {
     char title[100];
     char author[100];
     float price;
 };
-int main(){
+
+int main() {
     int n;
-    scanf("%d\n",&n);
+    scanf("%d", &n);
 
     struct Book book[n];
 
-    for(int i=0;i<n;i++){
-        scanf("%s %s %f",&book[i].title,&book[i].author,&book[i].price);
+    for (int i = 0; i < n; i++) {
+        scanf("%s %s %f", book[i].title, book[i].author, &book[i].price);
     }
 
     float threshold;
-    scanf("%f",&threshold);
-    int found=0;
-    printf("Books above price %.2f:\n",threshold);
-    for(int i=0;i<n;i++){
-        if(threshold<book[i].price){
-            threshold=book[i].price;
-            found=1;
-            break;
-        printf("Title: %s, Author: %s, Price: %.2f\n",book[i].title,book[i].author,book[i].price);
-    }}
+    scanf("%f", &threshold);
+
+    int found = 0;
+    printf("Books above price %.2f:\n", threshold);
+    for (int i = 0; i < n; i++) {
+        if (book[i].price > threshold) {
+            printf("Title: %s, Author: %s, Price: %.2f\n",
+                   book[i].title, book[i].author, book[i].price);
+            found = 1;
+        }
+    }
+
+    if (!found) {
+        printf("No books found above the given price.\n");
+    }
+
     return 0;
 }
+
 
